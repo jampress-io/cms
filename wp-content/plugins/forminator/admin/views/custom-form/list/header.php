cae8694e89dc6ffba3062a5f295fe85d971445f8
Damien McDonnell <dm@cct-marketing.com>
2021-06-03 07:36:13 +0100

    Removed unsupported form plugins

--- wp-content/plugins/forminator/admin/views/custom-form/list/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-$title         = __( 'Forms', 'forminator' );
-$create_dialog = 'custom_forms';
-$import_dialog = 'import_cform';
-$hash          = '#forms';
-
-require_once forminator_plugin_dir() . 'admin/views/common/list/header.php';
